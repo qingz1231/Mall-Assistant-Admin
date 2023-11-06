daa03f6f5c79c07b2a2dac449ec0812d09093011
Debbie <debbielow99@gmail.com>
2023-11-06 14:05:26 +0800

    CRUD

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from pymysql import connections
 import os
 from config import *
 from sql import *
+from ipregistry import IpregistryClient
+
 
 app = Flask(__name__)
 app.secret_key = 'admin'
@@ -40,6 +42,43 @@ def Error():
 @app.route("/Home")
 def Home():
     return render_template('index.html')
+
+@app.route("/EditAdmin/<string:id>", methods=['GET','POST'])
+def EditAdmin(id):
+
+    cursor = db_conn.cursor()
+
+    if(request.method == 'GET'):
+
+
+        try:
+            cursor.execute(getAdminById,(id))
+            
+            data = cursor.fetchone()
+  
+            if(data):
+ 
+                return render_template('editAdmin.html',data = data)
+
+            cursor.close()
+        except Exception as e:
+            print(e.__traceback__)
+            cursor.close()
+            return redirect(url_for('Error'))
+        
+    elif(request.method == 'POST'):
+        print(id)
+        print("post")
+        edit_password = request.form['password']
+        new_role = request.form.getlist('permission-item')
+        edit_role = "{" + ", ".join(new_role ) + "}"
+        print(edit_role)
+        
+        cursor.execute(updateAdminDetail,(edit_password,edit_role,"7"))
+        db_conn.commit()
+
+        return redirect(request.referrer)
+
 
 @app.route("/Admin")
 def Admin():
@@ -95,6 +134,15 @@ def LoginAdmin():
         if(data):
             flash('Login succesful')
             session["user"] = data
+            
+
+            # client = IpregistryClient("checkin")  
+            # ipInfo = client.lookup() 
+            # ip = ipInfo.ip
+            # print(ipInfo)
+
+            # cursor.execute(updateAdminDevice,(ip,data[0]))
+            # db_conn.commit()
             cursor.close()
             return redirect(url_for('Home'))
         else:
@@ -103,12 +151,59 @@ def LoginAdmin():
             return redirect(url_for('Login'))
         
     except Exception as e:
+        print(e.__cause__)
         cursor.close()
         return redirect(url_for('Error'))
         
     
 
-   
+@app.route("/AddAdmin", methods=['POST'])
+def AddAdmin():
+    register_id = request.form['username']
+    register_password = request.form['password']
+    checked_permission = request.form.getlist('permission-item')
+    register_permission = "{" + ", ".join(checked_permission ) + "}"
+
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(addAdmin,(1,register_id,register_password,register_permission))
+        flash('Admin Added Successfully')
+        db_conn.commit()
+        cursor.close()
+
+        return redirect(url_for('Admin'))
+
+        
+    except Exception as e:
+        cursor.close()
+        print(e.__cause__)
+        return redirect(url_for('Error'))
+        
+    
+@app.route("/DeleteAdmin/<string:id>")
+def DeleteAdmin(id):
+    
+
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(deleteAdmin,(id))
+        flash('Admin Added Successfully')
+        db_conn.commit()
+        cursor.close()
+
+        client = IpregistryClient("tryout")  
+        ipInfo = client.lookup() 
+        print(ipInfo)
+
+        return redirect(url_for('Admin'))
+
+        
+    except Exception as e:
+        cursor.close()
+        print(e.__cause__)
+        return redirect(url_for('Error'))
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
--- sql.py
+++ sql.py
@@ -1,8 +1,11 @@
 admin_table  = "admin"
 getAdminByLogin = "SELECT * FROM " + admin_table + " WHERE username=%s AND password=%s"
-getAdminById = "SELECT * FROM " + admin_table + " WHERE id=%s"
+getAdminById = "SELECT * FROM " + admin_table + " WHERE user_id=%s"
 getAllAdmin = "SELECT * FROM " + admin_table
-addAdmin = "INSERT INTO " + admin_table + " (username, password, role)" + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+addAdmin = "INSERT INTO " + admin_table + " (mall_id, username, password, role)" + " VALUES (%s, %s, %s, %s)"
+deleteAdmin = "DELETE FROM " + admin_table + " WHERE user_id = %s"
+updateAdminDevice = "UPDATE " + admin_table + " SET device = %s WHERE user_id = %s"
+updateAdminDetail = "UPDATE " + admin_table + " SET password = %s, role = %s WHERE user_id = %s"
 
 shop_table = "shop"
 getShopById = "SELECT * FROM " + shop_table + " WHERE id=%s"
